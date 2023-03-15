00f4f161a26a5f5a38f32d0b0ba756cc56bc0254
NicolasMack <127933971+NicolasMack@users.noreply.github.com>
2023-03-15 05:23:11 -0300

    Add files via upload

---
+++ Ex1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int menor(int i, int j, int ma[i][j]){
+    int menor;
+    int m;
+    int n;
+    menor = ma[0][0];
+    for(m=0;m<i;m++){
+        for(n=0;n<j;n++){
+            if (menor>ma[m][n]){
+                menor = m;
+            }
+        }
+    }
+    printf("\nLinha com o menor valor: %d", menor);
+    return menor;
+}
+
+int lervals(int i, int j, int ma[i][j]){
+    printf("\nDigite os valores da matriz: \n");
+    int m;
+    int n;
+    int mn;
+    for(m=0;m<i;m++){
+        for(n=0;n<j;n++){
+            scanf("%i", &ma[m][n]);
+        }
+    }
+    printf("\nMATRIZ:\n");
+    for(m=0;m<i;m++){
+        for(n=0;n<j;n++){
+            printf("%d ", ma[m][n]);
+        }
+        printf("\n");
+    }
+    mn = menor(i, j, ma);
+    return ma[i][j];
+}
+
+int main()
+{
+    int tam;
+    printf("Digite o tamanho da matriz quadrada: ");
+    scanf("%i", &tam);
+    int mquad[tam][tam];
+    int y;
+    y = lervals(tam, tam, mquad);
+    return 0;
+}
