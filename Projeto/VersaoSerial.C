92d0a2d59ae1fdcde18994a8ccdb71ccababdc9f
NicolasMack <127933971+NicolasMack@users.noreply.github.com>
2023-05-02 18:05:18 -0300

    Create VersaoSerial.C

---
+++ Projeto/VersaoSerial.C
@@ -0,0 +1,21 @@
+# Versao Serial do projeto
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int i, n;
+    float x, resultTaylor;
+    
+    //variaveis, sendo i o numero maximo de termos
+    printf("digite o numero de termos: ");
+    scanf("%d", &i);
+    
+    resultTaylor = 0;
+    for (n = 0; n <= i; n++) {
+        resultTaylor += pow(1, n) / tgamma(n + 1);
+        
+    }
+    printf("Polinomio: %.4f\n", resultTaylor);
+    
+    return 0;
+}
