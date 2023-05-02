b02da09be8478f27560bd4a659ced3f8fd05b476
NicolasMack <127933971+NicolasMack@users.noreply.github.com>
2023-05-02 18:49:34 -0300

    Update VersaoSerial.C

--- Projeto/VersaoSerial.C
+++ Projeto/VersaoSerial.C
@@ -1,19 +1,26 @@
-//Versao Serial do projeto
-
 #include <stdio.h>
-#include <math.h>
+
+int fatorial(int n) {
+    int i, x = 1;
+    for (i = 1; i <= n; i++) {
+        x *= i;
+    }
+    return x;
+}
 
 int main() {
     int i, n;
-    float x, resultTaylor;
+    float potencia, resultTaylor;
     
-    //variaveis, sendo i o numero de iterações
-    printf("digite o numero de termos: ");
+    // variaveis, sendo i o numero maximo de termos
+    printf("digite o numero de iteracoes: ");
     scanf("%d", &i);
     
     resultTaylor = 0;
+    potencia = 1;
     for (n = 0; n <= i; n++) {
-        resultTaylor += pow(1, n) / tgamma(n + 1);
+        resultTaylor += potencia / fatorial(n);
+        potencia *= 1;
         
     }
     printf("Polinomio: %.4f\n", resultTaylor);
